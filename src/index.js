7fa4e40648a81d9c5100a84d0f2620e8a35d1f04
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-14 11:06:00 +0300

    feat:  add my own code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+ if (validateArray(array)) {
+    array.sort(function(a, b) {
+        return a - b;
+      });
+        return array[0];}
+    else
+        return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (validateArray(array)) {
+    array.sort(function(a, b) { return a - b;});
+        return array[array.length-1];
+    }
+    else
+        return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+      if (validateArray(array)) {
+    let sum = 0;
+    array.forEach(element => { sum += element;});
+        return sum / array.length; }
+    else
+        return 0;
+}
+
+
+function validateArray(array){
+if (array.lenght != 0 || array != false){
+    return true;
+}
+else
+    return false;
 }
